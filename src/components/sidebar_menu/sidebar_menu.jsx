0515df95a6e43032476d844362c914dc6b65adf5
김영일 <rladuddlf3@naver.com>
2021-01-08 17:30:03 +0900

    컨트랙트와 통신하여 토큰수량 가져오기

--- src/components/sidebar_menu/sidebar_menu.jsx
+++ src/components/sidebar_menu/sidebar_menu.jsx
@@ -2,9 +2,16 @@ import React, {useEffect, useState} from "react";
 import styles from "./sidebar_menu.module.css";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
+import caver from "../../klaytn/caver";
+import * as config from "../../config";
+
+const DEPLOYED_ADDRESS = config.DEPLOYED_ADDRESS;
+const DEPLOYED_ABI = config.DEPLOYED_ABI;
 
 const SideBarMenu = () => {
+  const contract = new caver.klay.Contract(DEPLOYED_ABI, DEPLOYED_ADDRESS);
   const [wallet, setWallet] = useState(null);
+  const [tokenBalance, setTokenBalance] = useState(null);
   const history = useHistory();
 
   const onLogout = () => {
@@ -17,15 +24,22 @@ const SideBarMenu = () => {
     window.location.reload();
   };
 
-  useEffect(() => {
-    const fetchWallet = async () => {
-      try {
-        const respons = await axios.get("/api/users/wallet");
-        setWallet(respons.data.walletAddress);
-      } catch (e) {
-        console.log(e)
-      }
+  const fetchWallet = async () => {
+    try {
+      //KAS SDK를 활용하여 만든 지갑주소를 백엔드에서 가져오기
+      const respons = await axios.get("/api/users/wallet");
+      setWallet(respons.data.walletAddress);
+      // 콜 데이터를 통한 토큰 수량 가져오기
+      contract.methods.balanceOf(wallet).call().
+      then(balance => {
+      setTokenBalance(balance);
+    })
+    } catch (e) {
+      console.log(e)
     }
+  }
+  
+  useEffect(() => {
     fetchWallet();
   });
   return(
@@ -35,7 +49,7 @@ const SideBarMenu = () => {
         <div className={styles.address}>
         {wallet? <h3 className={styles.name}>{wallet}</h3> : <button onClick={addWallet}>지갑 생성</button>}
         </div>
-        <p className={styles.myToken}>보유 토큰 : 100 PTT</p>
+        <p className={styles.myToken}>보유 토큰 : {tokenBalance}</p>
         <button className={styles.logout} onClick={onLogout}>로그 아웃</button>
         <h4>내 컬러데이터 시편 현황</h4>
         <hr className={styles.colorBar1}/>
